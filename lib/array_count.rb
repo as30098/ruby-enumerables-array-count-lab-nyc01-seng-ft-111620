aefa77124fb8aef986819d6d508f54ffd3c3df12
Alexandra Spencer <alexspencer421@gmail.com>
2020-11-09 02:28:27 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -13,4 +13,13 @@ end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  total = 0
+index = 0
+while index < array.count do
+  if array[index].class == "" 
+    total += 1 
+  end
+  index += 1
+end
+total
 end
